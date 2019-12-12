8f44179f51c15575db4f9946fba81206137e2e52
ArunBhaskaran <58020161+ArunBhaskaran@users.noreply.github.com>
2019-12-12 12:12:16 +0530

    Update and rename Abst.java to Abstract.java

--- Abstract.java
+++ Abstract.java
@@ -1,18 +1,22 @@
  package sampleswing;
-abstract class Animal{
+ abstract class Animal
+ {
 	private String name;
-	public Animal(String name) {
+	public Animal(String name) 
+	{
 		this.name=name;
 		
 	}
 	public abstract void eat();
 	public abstract void breathe();
-	public String getName() {
+	public String getName() 
+	{
 		return name;
 	
 	}
 }
-class Racoon extends Animal{
+class Racoon extends Animal
+{
 	public Racoon (String name)
 	{
 	super (name);
@@ -26,7 +30,8 @@ class Racoon extends Animal{
 	}
 	}
 	
-	public class Abst {
+	public class Abst
+	{
 		public static void main(String [] args)
 		{
 			Racoon recoon=new Racoon("Mr.Bean");
