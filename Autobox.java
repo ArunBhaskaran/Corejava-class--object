531c56587c9b5f78a8cdf6d87db6dba5833d9bf7
ArunBhaskaran <58020161+ArunBhaskaran@users.noreply.github.com>
2019-12-12 12:19:37 +0530

    wrapper

---
+++ Autobox.java
@@ -0,0 +1,15 @@
+package wrapping;
+
+public class Autobox {
+	 
+		public static void main(String args[])
+		{  
+ 
+		Integer a=20;  
+		
+		Integer j=a; 
+		  
+		System.out.println(a+"  "+j);  
+		}}  
+
+
---
+++ Unbox.java
@@ -0,0 +1,12 @@
+package wrapping;
+
+public class Unbox {
+	public static void main(String args[])
+	{  
+    Integer a=new Integer(10);
+	int i=a.intValue();  
+	Integer j=i;  
+	System.out.println(a+" "+i+" "+j);  
+	}}
+
+
